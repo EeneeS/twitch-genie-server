1d48d16b2858c70124a65616de6e3e346b3082cb
Niels Soete <soeteniels1803@gmail.com>
2025-01-28 18:20:51 +0100

    basic structure

---
+++ cmd/api/api.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	address string
+}
+
+func (app *application) mount() *http.ServeMux {
+	mux := http.NewServeMux()
+	return mux
+}
+
+func (app *application) run(mux *http.ServeMux) error {
+	server := &http.Server{
+		Addr:         app.config.address,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+	log.Printf("Server has started at %v", app.config.address)
+	return server.ListenAndServe()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/eenees/twitch-genie-server
+
+go 1.23.5

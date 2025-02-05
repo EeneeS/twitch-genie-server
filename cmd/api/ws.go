1745f9756d56bef186fe4c269d9e76fb2276acfc
Niels Soete <soeteniels1803@gmail.com>
2025-02-05 13:24:01 +0100

    get channel id from query

--- cmd/api/ws.go
+++ cmd/api/ws.go
@@ -1,38 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gorilla/websocket"
-	"net/http"
+  "fmt"
+  "net/http"
+  "github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true // FIX: change
-	},
+  CheckOrigin: func(r *http.Request) bool {
+    return true // FIX: change
+  },
 }
 
 // WebSocket handler
 func (app *application) wsHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println("WebSocket upgrade error:", err)
-		return
-	}
-	defer conn.Close()
 
-	for {
-		_, msg, err := conn.ReadMessage()
-		if err != nil {
-			fmt.Println("Read error:", err)
-			break
-		}
+  channelId := r.URL.Query().Get("channel_id")
+  if channelId == "" {
+    http.Error(w, "missing channel id", http.StatusBadRequest)
+  }
 
-		fmt.Printf("Received: %s\n", msg)
+  conn, err := upgrader.Upgrade(w, r, nil)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusBadRequest)
+    return
+  }
+  defer conn.Close()
 
-		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
-			fmt.Println("Write error:", err)
-			break
-		}
-	}
+  for {
+    _, msg, err := conn.ReadMessage()
+    if err != nil {
+      fmt.Println("Read error:", err)
+      break
+    }
+
+    fmt.Printf("Received: %s\n", msg)
+
+    if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+      fmt.Println("Write error:", err)
+      break
+    }
+  }
 }

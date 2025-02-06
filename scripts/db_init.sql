1322628a13cf30727940f5420f2df3c489eb7cf1
Niels Soete <soeteniels1803@gmail.com>
2025-02-06 20:39:45 +0100

    remove db_init.sql

--- scripts/db_init.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE twitch-genie;
-
-CREATE TABLE IF NOT EXISTS users (
-  user_id varchar(255) PRIMARY KEY,
-  login varchar(255) NOT NULL,
-  access_token text,
-  refresh_token text
-);

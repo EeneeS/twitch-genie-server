0bbad59ce7d3b1f6db4253131fc31ed8466ae412
Niels Soete <soeteniels1803@gmail.com>
2025-02-06 11:08:52 +0100

    create basic psql script

---
+++ scripts/db_init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS twitch-genie;
+
+CREATE TABLE IF NOT EXISTS users (
+  user_id varchar(255) PRIMARY KEY,
+  login varchar(255) NOT NULL,
+  access_token text,
+  refresh_token text
+);
